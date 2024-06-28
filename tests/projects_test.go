ceb5f98dbb19387e9b7a4198f0b1d96081ce3f92
LekoLabs-AhmadAlkadri <ahmad@lekolabs.com>
2024-06-28 11:18:51 +0200

    ENGINE-482 Add test utilities to check if Redmine exists; add test to get a single project based on project identifier; add conditionals to not do the tests if the host not existing

--- tests/projects_test.go
+++ tests/projects_test.go
@@ -1,23 +1,12 @@
 package redmine_test
 
 import (
-	"fmt"
-	"os"
 	"testing"
 
-	"github.com/joho/godotenv"
 	"github.com/stretchr/testify/assert"
 
 	lkredmine "github.com/LekoLabs/go-redmine"
 )
-
-func Load_Dotenv(t *testing.T) {
-	if err := godotenv.Load(); err != nil {
-		t.Error("Dotenv file not found")
-	} else {
-		t.Log("Dotenv file found; environment variables loaded")
-	}
-}
 
 func createSingleProject(t *testing.T, project_id string, client_redmine *lkredmine.Client) {
 	preparedProject := lkredmine.ProjectToCreate{
@@ -28,37 +17,57 @@ func createSingleProject(t *testing.T, project_id string, client_redmine *lkredm
 	}
 	project, err := client_redmine.CreateProject(preparedProject)
 	assert.Nil(t, err, "Found the following error when trying to create the project:", err)
-	fmt.Printf("Succeeded in creating the following project: %v", project)
+	// fmt.Printf("Succeeded in creating the following project: %v", project)
+	_ = project
+}
+
+func getSingleProject(t *testing.T, project_id string, client_redmine *lkredmine.Client) {
+	project, err := client_redmine.Project(project_id)
+	assert.Nilf(t, err, "Found the following error when trying to fetch project %s: %v", project_id, err)
+	assert.NotEmpty(t, project, "Project supposed to be not empty.")
+	// fmt.Printf("Project found: %v", project)
+	_ = project
 }
 
 func deleteSingleProject(t *testing.T, project_id string, client_redmine *lkredmine.Client) {
-	Load_Dotenv(t)
+	loadEnvsFromDotenv(t)
 	err := client_redmine.DeleteProject(project_id)
 	assert.Nil(t, err, "Found the following error when trying to delete the project:", err)
-	fmt.Printf("Succeeded in deleting the following project: %s", project_id)
+	// fmt.Printf("Succeeded in deleting the following project: %s", project_id)
 }
 
 func Test_CreateDeleteSingleProject(t *testing.T) {
-	Load_Dotenv(t)
-
-	client_redmine := lkredmine.NewClient(
-		os.Getenv("REDMINE_HOST"),
-		os.Getenv("REDMINE_API_KEY"),
-	)
-
-	project_id := "test-project-x"
-	createSingleProject(t, project_id, client_redmine)
-	deleteSingleProject(t, project_id, client_redmine)
+	loadEnvsFromDotenv(t)
+	stat, host, apik := getRedmineHostApikFromEnvs()
+	if stat {
+		if doesRedmineHostExist(host) {
+			clientRedmine := lkredmine.NewClient(host, apik)
+			project_id := "test-project-x"
+			createSingleProject(t, project_id, clientRedmine)
+			getSingleProject(t, project_id, clientRedmine)
+			deleteSingleProject(t, project_id, clientRedmine)
+			return
+		}
+		t.Log("Environment variables found but redmine host not detected; test not run.")
+		return
+	}
+	t.Log("Environment variables not found; test not run.")
 }
 
 func Test_GetListProjects(t *testing.T) {
-	Load_Dotenv(t)
-
-	CLIENT_REDMINE := lkredmine.NewClient(
-		os.Getenv("REDMINE_HOST"),
-		os.Getenv("REDMINE_API_KEY"),
-	)
-	projects, err := CLIENT_REDMINE.Projects()
-	assert.Nil(t, err, "Found the following error when trying to get projects:", err)
-	fmt.Printf("Found the following projects: %v", projects)
+	loadEnvsFromDotenv(t)
+	stat, host, apik := getRedmineHostApikFromEnvs()
+	if stat {
+		if doesRedmineHostExist(host) {
+			clientRedmine := lkredmine.NewClient(host, apik)
+			projects, err := clientRedmine.Projects()
+			assert.Nil(t, err, "Found the following error when trying to get projects:", err)
+			// fmt.Printf("Found the following projects: %v", projects)
+			_ = projects
+			return
+		}
+		t.Log("Environment variables found but redmine host not detected; test not run.")
+		return
+	}
+	t.Log("Environment variables not found; test not run.")
 }
